c24dc148cb99206a3b35e08d58cb3b77f7f2a585
friutyguru <38260659+friutyguru@users.noreply.github.com>
2019-11-25 16:51:32 -0500

    data.js
    Data file that will hold user information 
    Has a sample user with the username testerjester
    To be updated

---
+++ data.js
@@ -0,0 +1,31 @@
+/******************************
+Data file for user information
+*******************************/
+
+const userData=[
+    {
+        username: "testerJester",
+        email: "fake@email.unreal",
+        password: "password",
+        artist1: "fakeArtist1",
+        artist2: "fakeArtist2",
+        artist3: "fakeArtist3",
+        genre1: "fakeGenre1",
+        genre2: "fakeGenre2",
+        genre3: "fakeGenre3",
+        raingenre: "fakeRainGenre",
+        workout1: "fakeWorkout1",
+        workout2: "fakeWorkout2",
+        workout3: "fakeWorkout3",
+        sadboi: "fakeSadboi",
+        firstsong: "fakeFirstSong",
+        momsong: "fakeMomSong",
+        underalbum: "fakeUnderAlbum",
+        guiltygenre: "fakeGuiltyGenre",
+        concertartist: "fakeConcertArtist",
+        leastfav: "fakeLeastFav"
+
+
+    }
+
+]
