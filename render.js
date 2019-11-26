2068ae3e9bf83131081f2e285f4a502ca3a1fbe4
manukrishnamurthy <36338922+manukrishnamurthy@users.noreply.github.com>
2019-11-26 14:21:13 -0500

    Update render.js

--- render.js
+++ render.js
@@ -43,16 +43,178 @@ function getInfo() {
     person.guiltygenre = guiltygenre;
     person.concertartist = concertartist;
     person.leastfav = leastfav;
+    for(let j = 0; j<userData.length; j++){
+        //check to make sure that account doesn't already exist
+    }
+    userData.append(person);
     // Append the 'person' object to the data.js file with all of the users. Not sure how to link
 
 }
+export const renderhomepage=function(){
+    return `
+    <section class="section has-background-primary" >
+    <div class="columns">
+            <div class="column">
+                <h1 class="title has-text-white">Signup</h1>
+                <form>
+                    <div class="field">
+                        <label class="label">Username</label>
+                        <div class="control">
+                            <input class="input" type="text" id = "username" placeholder="Username" name="username" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Email</label>
+                      <div class="control">
+                          <input class="input" type="text" id = "email" placeholder="...@email.com" name="email" required>
+                      </div>
+                  </div>
+                    <div class="field">
+                        <label class="label">Password</label>
+                        <div class="control">
+                            <input class="input" type="password" id = "password" placeholder="password" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                        <label class="label">Repeat Password</label>
+                        <div class="control">
+                            <input class="input" type="password" placeholder="repeatedpassword" name="password" required>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="field">
+                        <label class="title has-text-white">Music Preference Survey</label>
+                        <br>
+                        <br>
+                        <p class="is-size-4 has-text-white">Top 3 artists</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "artist1" placeholder="Katy Perry" name="Artist1">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "artist2" placeholder="Taylor Swift" name="Artist2">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "artist3" placeholder="Metallica" name="Artist3">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Top 3 genres</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "genre1" placeholder="Indie Rock" name="Genre1">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "genre2" placeholder="Hip Hop" name="Genre2">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "genre3" placeholder="R&B" name="Genre3">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Rainy Day Genre</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "RainGenre" placeholder="Sadboi music" name="RainGenre">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Top 3 Go-To Workout Songs</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "workout1" placeholder="Hallelujah" name="workout1">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "workout2" placeholder="22" name="workout2">
+                        </div>
+                        <div class="control">
+                          <input class="input" type="text" id = "workout3" placeholder="Row Your Boat" name="workout3">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Sadboi Artist of Choice</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "sadboi" placeholder="Adele" name="Sadboi">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">First Song You Play When You Get the Aux</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "firstsong" placeholder="All Night" name="firstsong">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Song You Play When Your Mom is in the Room</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "momsong" placeholder="Feliz Navidad" name="momsong">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Underrated Album</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "underalbum" placeholder="Damn" name="underalbum">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Guilty Pleasure Genre</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "guiltygenre" placeholder="Bubblegum Pop" name="guiltygenre">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Artist whose Concert you Most Want to Attend</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "concertartist" placeholder="Anderson.Paak" name="concertartist">
+                        </div>
+                        <br>
+                        <p class="is-size-4 has-text-white">Least Favorite Genre</p>
+                        <div class="control">
+                          <input class="input" type="text" id = "leastfav" placeholder="Country" name="leastfav">
+                        </div>
+                        <br>
+                    </div>
 
-function login() {
-    // arr is a placeholder for an array with all the 'person' objects. Not sure how to link
-    for (i = 0; i < arr.length; i++) {
-        if (username == arr[i].username && password == arr[i].password) {
-            console.log("Logged in!")
-            // Link to login page
-        }
-    }
+                    <div class="field">
+                        <div class="control">
+                            <input class="button is-dark" id = "button" type="submit" onclick="getInfo()"/>
+                        </div>
+                    </div>
+                </form>
+            </div>
+          <div class="column">
+            <h1 class="title has-text-white">Login</h1>
+                <form>
+                    <div class="field">
+                        <label class="label">Username</label>
+                        <div class="control">
+                            <input class="input" type="text" id = "loginusername" placeholder="Username" name="username" required>
+                        </div>
+                    </div>
+                    <div class="field">
+                      <label class="label">Password</label>
+                      <div class="control">
+                          <input class="input" type="text" id = "loginpassword" placeholder="...@email.com" name="email" required>
+                      </div>
+                  </div>
+                  <div class="field">
+                      <div class="control">
+                          <input class="button is-dark" id = "loginsubmit" type="submit" onclick="getInfo()"/>
+                      </div>
+                  </div>
+                </form>
+          </div>
+        
+    </div>
+    </section>`
 }
+export const renderuserprofile = function(user){
+    //use user information from data.js or person object to construct user rpofile
+}
+export const handleloginsubmit = function(event){
+    //replaceWith(renderUserProfile(userData[whichever user it was]))
+}
+//on click submit button for login or signup - generate user profile - render html as jquery object, replace index with user profile object
+function main(){
+    window.$root = $('#root');
+    let homepage = renderhomepage();
+    $root.append(homepage)
+    $root.on("click", "#submit", handleloginsubmit);
+    //renders
+}
+main();
+// function login() {
+//     // arr is a placeholder for an array with all the 'person' objects. Not sure how to link
+//     for (i = 0; i < arr.length; i++) {
+//         if (username == arr[i].username && password == arr[i].password) {
+//             console.log("Logged in!")
+//             // Link to login page
+//         }
+//     }
+// }
