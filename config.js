5394907d292923807e1e59d706ebbdd8dad38353
friutyguru <38260659+friutyguru@users.noreply.github.com>
2019-12-10 16:25:58 -0500

    Create config.js
    this file contains the consumer and secret keys as well as the access token/secret key for export

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+module.exports = {
+    consumer_key: 'CfX5vJj9MU0QyMATmehCfkEDz',
+    consumer_secret: 'hCx2BBiPQQayJiExmAvOVBbF22EQFvs0aUCp8VqyXfC3Pe50zo',
+    access_token_key: '1062050059368456192-tWoZGS8aI98rJ0S8zF9jfkv6tiWef0 ',
+    access_token_secret: 'uYQoYB0jvlwEiAlNNXgRDyztL7lvdrS3XGlNaksDtM7em'
+}
