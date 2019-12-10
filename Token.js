64c954efaeaded469177157f59ba72a23efa0ae5
friutyguru <38260659+friutyguru@users.noreply.github.com>
2019-12-10 16:48:07 -0500

    Create Token.js
    Token.js to get the token? idk

---
+++ Token.js
@@ -0,0 +1,11 @@
+  
+let token = localStorage.getItem('token');
+
+export const getToken = () => {
+  return token;
+};
+
+export const setToken = (t) => {
+  token = t;
+  localStorage.setItem('token', t);
+};
