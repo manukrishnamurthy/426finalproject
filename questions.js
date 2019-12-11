a35e3f5fe0fda5f0c2745ef5a8513f2b73749013
manukrishnamurthy <36338922+manukrishnamurthy@users.noreply.github.com>
2019-12-11 16:32:57 -0500

    added rock and country questions

--- questions.js
+++ questions.js
@@ -72,4 +72,154 @@ const popQuestions =[
         },
         correctAnswer: "a"
     },
-]    
+] 
+const rockquestions = [
+    {question: "Who is the highest paid musician to perform at Woodstock?",
+    answers:{
+        a: "Jimi Hendrix",
+        b: "Elvis Pressly",
+        c: "Fats Domino",
+        d: "Chuck Berry"
+    },
+    correctAnswer: "a"
+
+},
+{question: "What band name was also a term used in the 40's for UFOs?",
+    answers:{
+        a: "The Beatles",
+        b: "The Doors",
+        c: "Foo Fighters",
+        d:"Metallica"
+    },
+    correctAnswer: "c"
+},
+{question: "What musician left his band to join the cult Children of God?",
+    answers:{
+        a: "Jeremy Spencer",
+        b: "Travis Barker",
+        c: "Malcolm Young",
+        d: "Eric Clapton"
+    },
+    correctAnswer: "a"
+},
+{question: "Which band name is made up of the first letter of each of its members' names?",
+    answers:{
+        a: "Metallica",
+        b: "ABBA",
+        c: "The Arctic Monkeys",
+        d: "Led Zeppelin"
+    },
+    correctAnswer: "b"
+},
+{question: "Which artist was told to 'stick to driving a truck' at an audition for a local band?",
+    answers:{
+        a: "Ray Charles",
+        b: "Elton John",
+        c: "Jimi Hendrix",
+        d: "Elivs Pressly"
+    },
+    correctAnswer: "d"
+},
+{question: "Who is the lead singer of U2?",
+    answers:{
+        a: "Paul McCartney",
+        b: "Bono",
+        c: "James Brown",
+        d: "Carl Perkins"
+    },
+    correctAnswer: "b"
+},
+{question: "What is the name of AC/DC's 7th Studio Album?",
+    answers:{
+        a: "Highway to Hell",
+        b: "Powerage",
+        c: "Back in Black",
+        d: "High Voltage"
+    },
+    correctAnswer: "a"
+},
+{question: "What was the name of Pink Floyd's debut album?",
+    answers:{
+        a: "Piper at the Gates of Dawn",
+        b: "The Dark Side of the Moon",
+        c: "The final cut",
+        d: "The wall"
+    },
+    correctAnswer: "a"
+},
+]
+const countryquestions = [
+    {question: "Who is the highest paid country singer?",
+    answers:{
+        a: "Garth Brooks",
+        b: "Kenny Chesney",
+        c: "Luke Bryan",
+        d: "Dolly Parton"
+    },
+    correctAnswer: "a"
+
+},
+{question: "What was Florida Georgia Line's debut single?",
+    answers:{
+        a: "Cruise",
+        b: "Body like a Back Road",
+        c: "Tip it Back",
+        d:"Get Your Shine On"
+    },
+    correctAnswer: "a"
+},
+{question: "Who was the first black performer at the Grand Ole Opry?",
+    answers:{
+        a: "DeFord Bailey",
+        b: "Tina Turner",
+        c: "Charley Porter",
+        d: "Ray Charles"
+    },
+    correctAnswer: "a"
+},
+{question: "What is Darius Rucker's most played songs ever?",
+    answers:{
+        a: "Hootie and the Blowfish",
+        b: "Wagon Wheel",
+        c: "Ripcord",
+        d: "Traveller"
+    },
+    correctAnswer: "b"
+},
+{question: "What city's culture is centered around country music?",
+    answers:{
+        a: "San Fransisco",
+        b: "New York City",
+        c: "Kansas City",
+        d: "Nashville"
+    },
+    correctAnswer: "d"
+},
+{question: "Finish the lyric: 'Yeeeeeeeeeeah' I'm gonna take my horse to the: ",
+    answers:{
+        a: "Veteranarian",
+        b: "Old town Road",
+        c: "Bar",
+        d: "Town for an old fashioned night out she deserves it."
+    },
+    correctAnswer: "b"
+},
+{question: "Who holds the record for the most performed country song of the year 2008-2010?",
+    answers:{
+        a: "Keith Urban",
+        b: "Taylor Swift",
+        c: "Johnny Cash",
+        d: "Blake Shelton"
+    },
+    correctAnswer: "b"
+},
+{question: "What character did Johnny Cash voice on the Simpson's",
+    answers:{
+        a: "a coyote",
+        b: "Homer",
+        c: "Marge",
+        d: "Billy"
+    },
+    correctAnswer: "a"
+},
+]
