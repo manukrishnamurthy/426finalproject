5a673ad5cd9cd0615639424b8f9e9623e96462b9
friutyguru <38260659+friutyguru@users.noreply.github.com>
2019-12-10 23:40:28 -0500

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,75 @@
+const popQuestions =[
+    {question: "What was the name of Ariana Grande's 2019 breakup album",
+        answers:{
+            a: "Thank U, Next",
+            b: "Sweetener",
+            c: "NASA",
+            d:"Break Up With Your Girlfriend, I'm Bored"
+        },
+        correctAnswer: "a"
+
+    },
+    {question: "Which Artist Released 'When We Are Asleep, Where Do We Go?'",
+        answers:{
+            a: "Justin Bieber",
+            b: "Dua Lipa",
+            c: "Billie Eilish",
+            d:"John Legend"
+        },
+        correctAnswer: "c"
+    },
+    {question: "What movie featured the song 'Sunflower' by Post Malone ft. Swae Lee>",
+        answers:{
+            a: "Spider-man: Into THe Spiderverse",
+            b: "Drive",
+            c: "Us",
+            d: "Toy Story 4"
+        },
+        correctAnswer: "a"
+    },
+    {question: "Finish the lyric: 'I've been dancing on top of cars and _____' (Sucker: The Jonas Brothers) ",
+        answers:{
+            a: "fumbling out of bars",
+            b: "stumbling out of bars",
+            c: "driving electric cars",
+            d: "flying rockets to mars"
+        },
+        correctAnswer: "b"
+    },
+    {question: "Which artist was featured alongside Marshmallo in Happier?",
+        answers:{
+            a: "Justin Bieber",
+            b: "John Mayer",
+            c: "Ed Sheeren",
+            d: "Bastille"
+        },
+        correctAnswer: "d"
+    },
+    {question: "Who is the lead singer of Maroon V?",
+        answers:{
+            a: "Blake Shelton",
+            b: "Adam Levine",
+            c: "Pete Davidson",
+            d: "Sean Kingston"
+        },
+        correctAnswer: "b"
+    },
+    {question: "What is the name of Taylor Swift's 7th Studio Album?",
+        answers:{
+            a: "Seven",
+            b: "1989",
+            c: "Her",
+            d: "Me!"
+        },
+        correctAnswer: "a"
+    },
+    {question: "Who wasconsidered one of 2019's Break Out Pop Artists?",
+        answers:{
+            a: "Lizzo",
+            b: "Billie Eilish",
+            c: "Charlie XCX",
+            d: "Camilla Cabella"
+        },
+        correctAnswer: "a"
+    },
+]    
