bfe7d801dfbdf732fb9bd834bcd4bcc249f25874
manukrishnamurthy <36338922+manukrishnamurthy@users.noreply.github.com>
2019-12-11 11:16:30 -0500

    Update render.js

--- render.js
+++ render.js
@@ -69,24 +69,22 @@ export const renderhomepage=function(){
     return `
     <section class="section has-background-primary" >
     <form id="homepage">
+      <h1 class="title has-text-white">Genre Search</h1>
 
-   <h1 class="title has-text-white">Genre Search</h1>
-    
-      <div class="columns">
-        <div class="column is-11">
-          <input class="input" type="search" id="txt-search" placeholder="Hip-hop">
-        </div>
-        <div class="column is-1">
-          <div class="field">
-            <div class="control">
-                <input class="button is-dark" id = "genresubmit" type="submit" />
+        <div class="columns">
+          <div id="genresearchhomepage" class="column is-11">
+            <input class="input" type="search" id="txt-search" placeholder="Hip-hop">
+          </div>
+          <div class="column is-1">
+            <div class="field">
+              <div class="control">
+                  <input class="button is-dark" id = "genresubmit" type="submit" />
+              </div>
             </div>
           </div>
+            <label class="label" id="output"></label>
+            <label class="label" id="matches"></label>
         </div>
-          <label class="label" id="output"></label>
-          <label class="label" id="matches"></label>
-      </div>
-    
 
     <div class="columns">
             <div class="column">
@@ -195,6 +193,7 @@ export const renderhomepage=function(){
                         </div>
                         <br>
                     </div>
+
                     <div class="field">
                         <div class="control">
                             <input class="button is-dark" id = "signupsubmit" type="submit" />
@@ -204,7 +203,7 @@ export const renderhomepage=function(){
             </div>
           <div class="column">
             <h1 class="title has-text-white">Login</h1>
-                <form>
+                <form id="loginform">
                     <div class="field">
                         <label class="label">Username</label>
                         <div class="control">
@@ -214,7 +213,7 @@ export const renderhomepage=function(){
                     <div class="field">
                       <label class="label">Password</label>
                       <div class="control">
-                          <input class="input" type="text" id = "loginpassword" placeholder="...@email.com" name="email" required>
+                          <input class="input" type="password" id = "loginpassword" name="loginpassword" required>
                       </div>
                   </div>
                   <div class="field">
@@ -288,135 +287,134 @@ export const renderquizpop = function(){
   </section>
   </div>`
 }
-
 export const renderuserprofile = function(user){
-    //use user information from data.js or person object to construct user rpofile
-    return`<div id="userprofile">
-    <div class="columns">
-      <div class="column is-four-fifths">  
-        <h1 class="title is-1">Welcome ${user.username}!</h1>
-      </div>
-      <div class="column">
-        <button class="button is-medium" style="primary" data-id="${user.id}" id="signout" type="">Sign Out</button>
-      </div>
-    </div>
-    <section class="section">
-    <div class="container">
-    <h1 class="title is-1 has-text-white">Your Music Profile</h1>
-    <div class="columns">
-      <div class="column">  
-        <div class="content">
-          <h1 class="title">Top Artists</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.artist1}</li>
-              <li class="">${user.artist2}</li>
-              <li class="">${user.artist3}</li>
-            </ol>
+        //use user information from data.js or person object to construct user rpofile
+        return`<div id="userprofile">
+        <div class="columns">
+          <div class="column is-four-fifths">  
+            <h1 class="title is-1">Welcome ${user.username}!</h1>
+          </div>
+          <div class="column">
+            <button class="button is-medium" style="primary" data-id="${user.username}" id="signout" type="">Sign Out</button>
           </div>
         </div>
-        <div class="content">
-          <h1 class="title">Top Genres</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.genre1}</li>
-              <li class="">${user.genre2}</li>
-              <li class="">${user.genre2}</li>
-            </ol>
+        <section class="section">
+        <div class="container">
+        <h1 class="title is-1 has-text-white">Your Music Profile</h1>
+        <div class="columns">
+          <div class="column">  
+            <div class="content">
+              <h1 class="title">Top Artists</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.artist1}</li>
+                  <li class="">${user.artist2}</li>
+                  <li class="">${user.artist3}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Top Genres</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.genre1}</li>
+                  <li class="">${user.genre2}</li>
+                  <li class="">${user.genre2}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Rainy Day Genre</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.raingenre}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Top Go-To Workout Songs</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.workout1}</li>
+                  <li class="">${user.workout2}</li>
+                  <li class="">${user.workout3}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Saboi Artist of Choice</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.sadboi}</li>
+                </ol>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Rainy Day Genre</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.raingenre}</li>
-            </ol>
+          <div class="column">
+            <div class="content">
+              <h1 class="title">First Song You Play When You Get the Aux</h1>
+              <div class="subtitle">
+                <ol type="1">
+                 <li class="">${user.firstsong}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Song You Play When Your Mom is in the Room</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.momsong}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Underrated Album</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.underalbum}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Guilty Pleasure Genre</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.guiltygenre}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Artisit Whose Concert You Wanted to Attend</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.concertartist}</li>
+                </ol>
+              </div>
+            </div>
+            <div class="content">
+              <h1 class="title">Least Favorite Genre</h1>
+              <div class="subtitle">
+                <ol type="1">
+                  <li class="">${user.leastfav}</li>
+                </ol>
+              </div>
+            </div>
           </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Top Go-To Workout Songs</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.workout1}</li>
-              <li class="">${user.workout2}</li>
-              <li class="">${user.workout3}</li>
-            </ol>
           </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Saboi Artist of Choice</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.sadboi}</li>
-            </ol>
           </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="content">
-          <h1 class="title">First Song You Play When You Get the Aux</h1>
-          <div class="subtitle">
-            <ol type="1">
-             <li class="">${user.firstsong}</li>
-            </ol>
+          </section>
+          <section class="section">
+          <div class="container">
+          <h1 class="title is-2 has-text-white">Your Music Profile</h1>
+            <div class="content">
+              <button class="button is-medium" style="primary" data-id="${user.id}" id="renderquiz" type="submit">Take Quiz</button>
+            </div>
+            <div class="content">
+              <h2 class="title is-2">your quizzes</h2>
+            </div>
           </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Song You Play When Your Mom is in the Room</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.momsong}</li>
-            </ol>
-          </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Underrated Album</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.underalbum}</li>
-            </ol>
-          </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Guilty Pleasure Genre</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.guiltygenre}</li>
-            </ol>
-          </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Artisit Whose Concert You Wanted to Attend</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.concertartist}</li>
-            </ol>
-          </div>
-        </div>
-        <div class="content">
-          <h1 class="title">Least Favorite Genre</h1>
-          <div class="subtitle">
-            <ol type="1">
-              <li class="">${user.leastfav}</li>
-            </ol>
-          </div>
-        </div>
-      </div>
-      </div>
-      </div>
-      </section>
-      <section class="section">
-      <div class="container">
-      <h1 class="title is-2 has-text-white">Your Music Profile</h1>
-        <div class="content">
-          <button class="button is-medium" style="primary" data-id="${user.id}" id="renderquiz" type="submit">Take Quiz</button>
-        </div>
-        <div class="content">
-          <h2 class="title is-2">your quizzes</h2>
-        </div>
-      </div>
-      </section>
-    </div>`
+          </section>
+        </div>`
 }
 export const handleloginsubmit = async function(event){
   event.preventDefault();
@@ -451,21 +449,18 @@ export const handlesignout = async function(event){
   //axios request to backend to sign out
   $userprofile.replaceWith(renderhomepage);
 }
+export const handlechangepassword = async function(event){
+
+}
+
 export const handlerenderquiz = async function(event){
-  const $userprofile = $(event.target).closest('#homepage');
-  $userprofile.replaceWith(renderhomepage)
+  const $genresearch = $(event.target).closest('#txt-search');
+  // let infoarray = $genresearch.serializeArray();
+  
+  // console.log(infoarray);
 }
-
-export const handletakequiz = async function(event){
-  event.preventDefault();
-  const $user = $(event.target).closest('#userprofile');
- // let person = userprofile.person;
-  let quizpop = renderquizpop(); 
-  $user.replaceWith(quizpop);
-}
-
 export const handlesignupsubmit = async function(event){
-    event.preventDefault();
+  event.preventDefault();
   //getInfo();
     const $homepage = $(event.target).closest('#homepage');
     //console.log($homepage);
@@ -529,37 +524,39 @@ export const handlesignupsubmit = async function(event){
   }).catch(error => {
     console.log(error);
   });
-  // let h = axios.get('http://localhost:3000/user/'+person.username+'/',
-  //   {headers: { Authorization: "Bearer " + window.jwt }},);
-  // await h.then(response => {
-  //   console.log(response.data);
-  // }).catch(error => {
-  //   console.log(error);
-  // });
+  let h = axios.get('http://localhost:3000/account/status',
+    {headers: { Authorization: "Bearer " + window.jwt }},);
+  await h.then(response => {
+    console.log(response.data);
+  }).catch(error => {
+    console.log(error);
+  });
     let userprofile = renderuserprofile(person);
     
     $homepage.replaceWith(userprofile);
 }
 //on click submit button for login or signup - generate user profile - render html as jquery object, replace index with user profile object
-function main(){
+async function main(){
     window.$root = $('#root');
     let homepage = renderhomepage();
     $root.append(homepage)
+    // let j = axios.post('http://localhost:3000/public/questions',
+    // {
+    //   data: popQuestions
+    // });
+    // await j.then(response => {
+    //   console.log(response.data);
+    //   window.jwt = response.data.jwt;
+    // }).catch(error => {
+    //   console.log(error);
+    // });
     $root.on("click", "#loginsubmit", handleloginsubmit);
     $root.on("click", "#signupsubmit", handlesignupsubmit);
     $root.on("click", "#signout", handlesignout);
     $root.on("click", "#renderquiz", handlerenderquiz);
-    $root.on("click", "#renderquiz", handletakequiz);
-
-    //renders
+    $root.on("click", "#genresubmit", handlerenderquiz);
+   // $root.on("click", "#renderquiz", handletakequiz);
+    // $root.on("click", "#changepassword", handlechangepassword);
+    // $root.on("click", "#deleteaccount", handledeleteaccount);
 }
 main();
-// function login() {
-//     // arr is a placeholder for an array with all the 'person' objects. Not sure how to link
-//     for (i = 0; i < arr.length; i++) {
-//         if (username == arr[i].username && password == arr[i].password) {
-//             console.log("Logged in!")
-//             // Link to login page
-//         }
-//     }
-// }
